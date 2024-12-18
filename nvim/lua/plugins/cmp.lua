2e3d38a3652d16e197ab1bd8cb448a2c5e4258d3
Julian Baumgartner <jbaumgartner93@gmail.com>
2024-12-18 09:20:07 -0500

    Fix breaking chagnes

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "v0.*",
+    opts = function(_, opts)
+      opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
+        preset = "enter",
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      })
+
+      opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
+        list = {
+          selection = "auto_insert",
+        },
+      })
+
+      return opts
+    end,
+  },
+}
