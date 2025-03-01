1fd5d77a1d175ad476c2e705cd790d3e29b079c2
Julian Baumgartner <jbaumgartner93@gmail.com>
2025-02-28 21:39:32 -0500

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,9 @@
 return {
-  -- add gruvbox
   { "rmehri01/onenord.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "onenord",
     },
-  }
+  },
 }
