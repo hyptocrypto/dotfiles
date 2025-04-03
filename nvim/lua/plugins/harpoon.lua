6c042f81cc6a09154ffe090b6734020f313dd6b4
Julian Baumgartner <jbaumgartner93@gmail.com>
2025-04-03 14:47:44 -0400

    fix harpoooonanyy boi

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,11 +1,23 @@
 return {
   "ThePrimeagen/harpoon",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("harpoon").setup({
-      menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
-      },
-    })
-  end,
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  menu = {
+    width = vim.api.nvim_win_get_width(0) - 10,
+    keymaps = {
+      ["<Tab>"] = function(menu)
+        menu:select_next()
+      end,
+      ["<S-Tab>"] = function(menu)
+        menu:select_prev()
+      end,
+    },
+  },
+  config = true,
+  keys = {
+    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+    { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+  },
 }
