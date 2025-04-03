39207368c5bf65442b870f00085d5fbd299ad9e1
Julian Baumgartner <jbaumgartner93@gmail.com>
2025-04-03 14:51:18 -0400

    Update

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,20 +4,30 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  menu = {
-    width = vim.api.nvim_win_get_width(0) - 10,
-    keymaps = {
-      ["<Tab>"] = function(menu)
-        menu:select_next()
-      end,
-      ["<S-Tab>"] = function(menu)
-        menu:select_prev()
-      end,
-    },
-  },
   config = true,
   keys = {
-    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-    { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+    {
+      "<leader>ha",
+      function()
+        require("harpoon.mark").add_file()
+        vim.notify("File marked with Harpoon", vim.log.levels.INFO)
+      end,
+      desc = "Mark file with harpoon",
+    },
+    {
+      "<leader>hh",
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      desc = "Show harpoon marks",
+    },
+    {
+      "<leader>hc",
+      function()
+        require("harpoon.mark").clear_all()
+        vim.notify("Cleared all Harpoon marks", vim.log.levels.WARN)
+      end,
+      desc = "Clear all harpoon marks",
+    },
   },
 }
