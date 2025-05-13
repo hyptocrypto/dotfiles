0e96d92d3f85880bcb942cb94c4bdbeacfc3d95e
Julian Baumgartner <jbaumgartner93@gmail.com>
2025-05-13 17:27:28 -0400

    update harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,33 +1,54 @@
 return {
   "ThePrimeagen/harpoon",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = true,
-  keys = {
-    {
-      "<leader>ha",
-      function()
-        require("harpoon.mark").add_file()
-        vim.notify("File marked with Harpoon", vim.log.levels.INFO)
-      end,
-      desc = "Mark file with harpoon",
-    },
-    {
-      "<leader>hh",
-      function()
-        require("harpoon.ui").toggle_quick_menu()
-      end,
-      desc = "Show harpoon marks",
-    },
-    {
-      "<leader>hc",
-      function()
-        require("harpoon.mark").clear_all()
-        vim.notify("Cleared all Harpoon marks", vim.log.levels.WARN)
-      end,
-      desc = "Clear all harpoon marks",
-    },
-  },
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local status_ok, harpoon = pcall(require, "harpoon")
+    if not status_ok then
+      return
+    end
+    harpoon:setup()
+
+    -- picker
+    local function generate_harpoon_picker()
+      local file_paths = {}
+      for _, item in ipairs(harpoon:list().items) do
+        table.insert(file_paths, {
+          text = item.value,
+          file = item.value,
+        })
+      end
+      return file_paths
+    end
+    vim.keymap.set("n", "<leader>ha", function()
+      harpoon:list():add()
+      vim.notify("File marked with Harpoon", vim.log.levels.INFO)
+    end)
+    vim.keymap.set("n", "<leader>hh", function()
+      Snacks.picker({
+        finder = generate_harpoon_picker,
+        win = {
+          input = {
+            keys = {
+              ["dd"] = { "harpoon_delete", mode = { "n", "x" } },
+            },
+          },
+          list = {
+            keys = {
+              ["dd"] = { "harpoon_delete", mode = { "n", "x" } },
+            },
+          },
+        },
+        actions = {
+          harpoon_delete = function(picker, item)
+            local to_remove = item or picker:selected()
+            table.remove(harpoon:list().items, to_remove.idx)
+            picker:find({
+              refresh = true, -- refresh picker after removing values
+            })
+          end,
+        },
+      })
+    end)
+  end,
 }
